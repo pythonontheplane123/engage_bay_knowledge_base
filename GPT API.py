c1b093fe94b74de4db4977b2c6ae19b03224eb5d
Ak-shath123 <61642283+pythonontheplane123@users.noreply.github.com>
2023-03-24 06:45:49 +0530

    Create GPT API.py

---
+++ GPT API.py
@@ -0,0 +1,81 @@
+from fastapi import FastAPI
+from gpt_index import SimpleDirectoryReader, GPTListIndex, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
+import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000"
+    "http://localhost:8080",
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+os.environ["OPENAI_API_KEY"] = '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'
+directory = '/Users/macos/desktop/ze_knowledge_base 2'
+
+
+def construct_index(directory_path):
+    max_input_size = 4096
+    num_outputs = 512
+    max_chunk_overlap = 20
+    chunk_size_limit = 600
+
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.7, model_name="text-davinci-003", max_tokens=num_outputs))
+
+    documents = SimpleDirectoryReader(directory).load_data()
+
+    index = GPTSimpleVectorIndex(documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+
+    index.save_to_disk('index.json')
+
+    return index
+
+
+
+
+"""
+iface = gr.Interface(fn=chatbot,
+                     inputs=gr.inputs.Textbox(lines=7, label="Enter your text"),
+                     outputs="text",
+                     title="Custom-trained AI Chatbot")
+
+index = construct_index("docs")
+iface.launch(share=True)
+"""
+
+
+index = construct_index("docs")
+
+
+
+@app.get("/")
+def read_root():
+    return {"answer":"Hello World"}
+
+
+@app.get("/predict")
+def answergen(input_text):
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    response = index.query("Answer the question only based on the context " + input_text, response_mode="compact")
+    return response.response
+
+
+#uvicorn API:app --port 8000 --reload
